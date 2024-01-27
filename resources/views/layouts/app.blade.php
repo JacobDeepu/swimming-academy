0a73c241a87a45702452b906e184cb68ef5a7689
Jacob Deepu <d2jdeepu@gmail.com>
2024-01-27 18:52:37 +0530

    Change the main content area style
        Needed after adding sidebar 53e19b38

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,7 +29,7 @@
             @endif
 
             <!-- Page Content -->
-            <main>
+            <main class="h-auto p-4 pt-20 md:ml-64">
                 {{ $slot }}
             </main>
         </div>
