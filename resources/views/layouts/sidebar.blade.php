493a8e00b8738b9f0d75d74857f18bca7ba1f157
Jacob Deepu <d2jdeepu@gmail.com>
2024-01-27 18:44:18 +0530

    Create a sidebar for the layout

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,15 @@
+<aside class="fixed left-0 top-0 z-40 h-screen w-64 -translate-x-full border-r border-gray-200 bg-white pt-14 transition-transform md:translate-x-0">
+    <div class="h-full overflow-y-auto bg-white px-3 py-5">
+        <ul class="space-y-2">
+            <li>
+                <a class="group flex items-center rounded-lg p-2 text-base font-medium text-gray-900 hover:bg-gray-100" href="#">
+                    <svg class="h-6 w-6 text-gray-500 transition duration-75 group-hover:text-gray-900" aria-hidden="true" fill="currentColor" viewBox="0 0 256 256" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M152,208V160a8,8,0,0,0-8-8H112a8,8,0,0,0-8,8v48a8,8,0,0,1-8,8H48a8,8,0,0,1-8-8V115.54a8,8,0,0,1,2.62-5.92l80-75.54a8,8,0,0,1,10.77,0l80,75.54a8,8,0,0,1,2.62,5.92V208a8,8,0,0,1-8,8H160A8,8,0,0,1,152,208Z" />
+                    </svg>
+                    <span class="ml-3">Dashboard</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+</aside>
