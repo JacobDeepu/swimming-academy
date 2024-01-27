53e19b385a86619ef3281d2318c2ea83ddca37a1
Jacob Deepu <d2jdeepu@gmail.com>
2024-01-27 18:50:18 +0530

    Include sidebar in app layout
        Hide the header for now.
    
    TODO: Update it to include a breadcrumb.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,10 +17,11 @@
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
             @include('layouts.navigation')
+            @include('layouts.sidebar')
 
             <!-- Page Heading -->
             @if (isset($header))
-                <header class="bg-white shadow">
+                <header class="hidden bg-white shadow">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
