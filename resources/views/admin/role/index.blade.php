90ce6070e159e16d45d021c4b6a56aae8933bd53
Jacob Deepu <d2jdeepu@gmail.com>
2024-01-29 16:35:54 +0530

    role: Show options based on permissions

--- resources/views/admin/role/index.blade.php
+++ resources/views/admin/role/index.blade.php
@@ -25,14 +25,16 @@
                         </form>
                     </div>
                     <div class="flex w-full flex-shrink-0 flex-col items-stretch justify-end space-y-2 md:w-auto md:flex-row md:items-center md:space-x-3 md:space-y-0">
-                        <button
-                            class="flex items-center justify-center rounded-lg bg-primary-700 px-4 py-2 text-sm font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300"
-                            type="button">
-                            <svg class="mr-2 h-3.5 w-3.5" aria-hidden="true" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                <path clip-rule="evenodd" fill-rule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" />
-                            </svg>
-                            Create role
-                        </button>
+                        @can('create a role')
+                            <button
+                                class="flex items-center justify-center rounded-lg bg-primary-700 px-4 py-2 text-sm font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300"
+                                type="button">
+                                <svg class="mr-2 h-3.5 w-3.5" aria-hidden="true" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                    <path clip-rule="evenodd" fill-rule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" />
+                                </svg>
+                                Create role
+                            </button>
+                        @endcan
                     </div>
                 </div>
                 <div class="overflow-x-auto">
@@ -40,24 +42,27 @@
                         <thead class="bg-gray-50 text-xs uppercase text-gray-700">
                             <tr>
                                 <th class="px-4 py-3" scope="col">Role name</th>
-                                <th class="px-4 py-3" scope="col">Actions
-                                </th>
+                                @can('edit a role')
+                                    <th class="px-4 py-3" scope="col">Actions</th>
+                                @endcan
                             </tr>
                         </thead>
                         <tbody>
                             @forelse ($roles as $role)
                                 <tr class="border-b">
                                     <th class="whitespace-nowrap px-4 py-3 font-medium text-gray-900" scope="row">{{ $role->name }}</th>
-                                    <td class="flex items-center px-4 py-3">
-                                        <a class="inline-flex items-center rounded-lg p-0.5 text-center text-sm font-medium text-gray-500 hover:text-gray-800 focus:outline-none"
-                                            href="{{ route('role.edit', $role) }}">
-                                            <svg class="feather feather-check-square mr-1 h-4 w-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                                                stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
-                                                <polyline points="9 11 12 14 22 4"></polyline>
-                                                <path d="M21 12v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11"></path>
-                                            </svg> Edit
-                                        </a>
-                                    </td>
+                                    @can('edit a role')
+                                        <td class="flex items-center px-4 py-3">
+                                            <a class="inline-flex items-center rounded-lg p-0.5 text-center text-sm font-medium text-gray-500 hover:text-gray-800 focus:outline-none"
+                                                href="{{ route('role.edit', $role) }}">
+                                                <svg class="feather feather-check-square mr-1 h-4 w-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                                                    stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+                                                    <polyline points="9 11 12 14 22 4"></polyline>
+                                                    <path d="M21 12v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11"></path>
+                                                </svg> Edit
+                                            </a>
+                                        </td>
+                                    @endcan
                                 </tr>
                             @empty
                                 <tr class="border-b bg-white">
