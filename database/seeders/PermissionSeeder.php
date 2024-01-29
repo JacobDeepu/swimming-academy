255dd589a892e8f5f5a4ddbe23a1a874fe6d8fe2
Jacob Deepu <d2jdeepu@gmail.com>
2024-01-29 16:21:25 +0530

    Create a Permission seeder
        Seed permissions to manage Roles

---
+++ database/seeders/PermissionSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+
+class PermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $arrayOfPermissionNames = [
+            'view a role',
+            'create a role',
+            'update a role',
+            'delete a role'
+        ];
+        $permissions = collect($arrayOfPermissionNames)->map(function ($permission) {
+            return ['name' => $permission, 'guard_name' => 'web'];
+        });
+
+        Permission::insert($permissions->toArray());
+    }
+}
