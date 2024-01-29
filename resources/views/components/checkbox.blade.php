dcde349e7610727c5a62e54c24802ff6bae37908
Jacob Deepu <d2jdeepu@gmail.com>
2024-01-29 18:59:18 +0530

    Add checked prop to checkbox component

--- resources/views/components/checkbox.blade.php
+++ resources/views/components/checkbox.blade.php
@@ -1,1 +1,3 @@
-<input type="checkbox" {!! $attributes->merge(['class' => 'rounded border-gray-300 text-primary-600 shadow-sm focus:ring-primary-500']) !!}>
+@props(['checked' => false])
+
+<input type="checkbox" {{ $checked ? 'checked' : '' }} {!! $attributes->merge(['class' => 'rounded border-gray-300 text-primary-600 shadow-sm focus:ring-primary-500']) !!}>
