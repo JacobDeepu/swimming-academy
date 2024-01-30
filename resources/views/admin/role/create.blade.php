f3fe23a6254465c66cf934b57d51ce7839108d31
Jacob Deepu <d2jdeepu@gmail.com>
2024-01-30 21:10:27 +0530

    Remove page header
        Remove from all the pages.

--- resources/views/admin/role/create.blade.php
+++ resources/views/admin/role/create.blade.php
@@ -1,10 +1,4 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="text-xl font-semibold leading-tight text-gray-800">
-            {{ __('Create Role') }}
-        </h2>
-    </x-slot>
-
     <div class="py-12">
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div class="relative overflow-hidden bg-white shadow-md sm:rounded-lg">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,15 +18,6 @@
         <div class="min-h-screen bg-gray-100">
             @include('layouts.navigation')
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="hidden bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
-
             <!-- Page Content -->
             <main class="h-auto p-4 pt-20 md:ml-64">
                 {{ $slot }}
