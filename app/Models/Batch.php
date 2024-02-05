31c576db4f32af71408930417c7ca3be1acd66ff
Jacob Deepu <d2jdeepu@gmail.com>
2024-02-05 19:59:35 +0530

    Make model Batch with migration

---
+++ app/Models/Batch.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Batch extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'level',
+        'capacity_min',
+        'capacity_max',
+        'schedule_id',
+        'instructor_id',
+        'pool_id',
+    ];
+}
---
+++ database/migrations/2024_02_05_195109_create_batches_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('batches', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('level');
+            $table->integer('capacity_min');
+            $table->integer('capacity_max');
+            $table->foreignId('schedule_id')
+                ->cascadeOnDelete()
+                ->constrained();
+            $table->foreignId('instructor_id')
+                ->cascadeOnDelete()
+                ->constrained(table: 'users');
+            $table->foreignId('pool_id')
+                ->cascadeOnDelete()
+                ->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('batches');
+    }
+};
